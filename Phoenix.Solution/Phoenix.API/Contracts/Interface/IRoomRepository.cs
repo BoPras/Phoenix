ac893517578958c550a7e09032538ae1277fd1ee
admin <prastowowidayanto123@gmail.com>
2023-12-19 10:20:34 +0700

    init program

---
+++ Phoenix.Solution/Phoenix.API/Contracts/Interface/IRoomRepository.cs
@@ -0,0 +1,10 @@
+﻿using Phoenix.API.Model;
+
+namespace Phoenix.API.Contracts.Interface
+{
+    public interface IRoomRepository
+    {
+        List<RoomModel> GetRoomByNumber(string number);
+        List<RoomModel> GetRooms();
+    }
+}

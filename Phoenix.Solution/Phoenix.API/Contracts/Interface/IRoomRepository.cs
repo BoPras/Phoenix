dd4d733f1757aa6811bae3da0c2fd6d05d814849
pras <prastowowidayanto123@gmail.com>
2023-12-19 11:35:46 +0700

    adding POST and PUT room

--- Phoenix.Solution/Phoenix.API/Contracts/Interface/IRoomRepository.cs
+++ Phoenix.Solution/Phoenix.API/Contracts/Interface/IRoomRepository.cs
@@ -4,7 +4,9 @@ namespace Phoenix.API.Contracts.Interface
 {
     public interface IRoomRepository
     {
+        List<RoomModel> AddNewRoom(RoomModel room);
         List<RoomModel> GetRoomByNumber(string number);
         List<RoomModel> GetRooms();
+        List<RoomModel> UpdateRoomByNumber(string number, RoomModel room);
     }
 }
--- Phoenix.Solution/Phoenix.API/Controllers/RoomController.cs
+++ Phoenix.Solution/Phoenix.API/Controllers/RoomController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Phoenix.API.Contracts.Interface;
+using Phoenix.API.Model;
 
 namespace Phoenix.API.Controllers
 {
@@ -38,6 +39,36 @@ namespace Phoenix.API.Controllers
             catch (Exception e)
             {
                 return BadRequest(e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] RoomModel roomModel) 
+        {
+            try
+            {
+                var result = this._roomRepository.AddNewRoom(roomModel);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put(string number, [FromBody] RoomModel roomModel)
+        {
+            try
+            {
+                var result = this._roomRepository.UpdateRoomByNumber(number, roomModel);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, e.Message);
             }
         }
     }

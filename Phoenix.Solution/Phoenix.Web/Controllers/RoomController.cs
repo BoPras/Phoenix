870b19d5b762e6aee92d0edcd5e690303668c54f
pras <prastowowidayanto123@gmail.com>
2023-12-21 11:34:16 +0700

    room controller mvc

---
+++ Phoenix.Solution/Phoenix.Web/Controllers/RoomController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phoenix.Web.Models.Room;
+using System.Text.Json;
+
+namespace Phoenix.Web.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public RoomController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:5281");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("api/room");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responsdata = await response.Content.ReadAsStringAsync();
+                    var rooms = JsonSerializer.Deserialize<List<RoomViewModel>>(responsdata, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    });
+
+                    ViewData["Rooms"] = rooms;
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Error = "Failed to fetch data from the room API";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "An error occured: " + ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ Phoenix.Solution/Phoenix.Web/Models/Room/RoomViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Phoenix.Web.Models.Room
+{
+    public class RoomViewModel
+    {
+        public string? Number { get; set; }
+        public int? Floor { get; set; }
+        public string? Room_type { get; set; }
+        public int? Guest_Limit { get; set; }
+        public string? Description { get; set; }
+        public decimal? Cost { get; set; }
+    }
+}
